c937dbf144f1f5970ca85a29a10a5ca1ee3e4c38
Max Alekseenko <nnwen0609@gmail.com>
2020-09-18 15:05:22 +0300

    add address check

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,17 @@
 import { BigInt, Address, store, log } from '@graphprotocol/graph-ts';
 import { LiquidityToken, Transfer } from '../generated/LiquidityToken/LiquidityToken';
-import { Distribution, Distributed } from '../generated/Distribution/Distribution';
+import { AddressChecker } from '../generated/LiquidityToken/AddressChecker';
+import { Distributed } from '../generated/Distribution/Distribution';
 import { LiquidityProvider, CommonData, Round } from '../generated/schema';
 
 let vampireAttackContracts: Array<string> = [
   '0x0ec1f1573f3a2db0ad396c843e6a079e2a53e557', // Sake
 ];
 
-function updateBalance(address: Address, value: BigInt, increase: boolean): void {
+const addressCheckerAddress = Address.fromString('0xe46ec14432033ede29343e9692a32c9adc314271');
+const addressCheckerBlockNumber = BigInt.fromI32(10866458);
+
+function updateBalance(address: Address, value: BigInt, increase: boolean, blockNumber: BigInt): void {
   if (address.toHexString() == '0x0000000000000000000000000000000000000000') return;
   let id = address.toHex();
   let liquidityProvider = LiquidityProvider.load(id);
@@ -15,6 +19,12 @@ function updateBalance(address: Address, value: BigInt, increase: boolean): void
     liquidityProvider = new LiquidityProvider(id);
     liquidityProvider.address = address;
     liquidityProvider.balance = BigInt.fromI32(0);
+    let isContract = false;
+    if (blockNumber.gt(addressCheckerBlockNumber)) {
+      let addressCheckerInstance = AddressChecker.bind(addressCheckerAddress);
+      isContract = addressCheckerInstance.isContract(address);
+    }
+    liquidityProvider.isContract = isContract;
   }
   liquidityProvider.balance = increase ? liquidityProvider.balance.plus(value) : liquidityProvider.balance.minus(value);
   if (liquidityProvider.balance.isZero()) {
@@ -40,8 +50,8 @@ export function handleTransfer(event: Transfer): void {
     !vampireAttackContracts.includes(event.params.from.toHexString()) &&
     !vampireAttackContracts.includes(event.params.to.toHexString())
   ) {
-    updateBalance(event.params.from, event.params.value, false);
-    updateBalance(event.params.to, event.params.value, true);
+    updateBalance(event.params.from, event.params.value, false, event.block.number);
+    updateBalance(event.params.to, event.params.value, true, event.block.number);
   }
   updateTotalSupply(contract);
 }
